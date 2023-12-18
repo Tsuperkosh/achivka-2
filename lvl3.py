560fbd29762e6ca6a0e85344573f13703404f7ef
Кузнецов Вадим Дмитриевич <tsuperkosh@MacBook-Pro.local>
2023-12-18 18:18:15 +0300

    com4

--- lvl3.py
+++ lvl3.py
@@ -6,12 +6,11 @@
 
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+import logging
 from datetime import datetime
-import pymysql
-pymysql.install_as_MySQLdb()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:macho man1@localhost/mydatabase'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///numbers.db'
 db = SQLAlchemy(app)
 
 # Модель для хранения чисел
@@ -51,10 +50,8 @@ def process_number():
     # Увеличение числа
     return jsonify({'result': number + 1})
 
-
-
-#if __name__ == '__main__':
-app.run(debug=True, port=5000)
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
 
 
 # In[ ]:
